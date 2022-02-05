f6bb784c35eee26da23691f84aa2e06bc9e28aa2
Christiaan Fourie <christiaanfourie60@gmail.com>
2022-02-05 20:29:10 +0200

    edits

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import csv
 # ****************************************************************************************
 # Variables for strategy
 currency_pair = 'ETHZAR'
-tick_period = 1 # <----- The tick interfal in seconds
+tick_ = 1 # <----- The tick interfal in seconds
 data_field = 10 # <----- The amount of rows to track for each second# Luno API Key
 
 # API Key & Secret
@@ -69,22 +69,8 @@ def handler(tick_raw):
     }
     
     matrix = pd.DataFrame(data)
+    print(matrix['average_ask'])
     
-    filename = "data.csv"
-    # opening the file with w+ mode truncates the file
-    f = open(filename, "w+")
-    f.close()
-
-
-    with open('data.csv', 'w', encoding='cp1251') as f:
-        writer = csv.writer(f)
-
-        writer.writerow(data)
-
-        # for row in data:
-        #     for i in row:  
-        #         writer.writerow(data[row])
-        #         print(data[row])
 
 
 
@@ -114,7 +100,7 @@ def main():
             print(e)
 
         # Wait for the next tick period
-        time.sleep(tick_period)
+        time.sleep(tick_)
 
 
 
@@ -122,106 +108,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def calculations(asks, bids):
-#     average_ask = int(sum(asks) / len(asks))
-#     average_bid = int(sum(bids) / len(bids))
-#     high_ask = max(asks)
-#     high_bid = max(bids)
-#     low_ask = min(asks)
-#     low_bid = min(bids)
-#     print(
-#         '  -> Selling Price average: ', average_ask,'\n',
-#         ' -> Buying Price average: ', average_bid,'\n',
-#         ' -> Highest Selling Price: ', high_ask,'\n',
-#         ' -> Highest Buying Price: ', high_bid,'\n',
-#         ' -> Lowest Selling Price: ', low_ask,'\n',
-#         ' -> Lowest Buying Price: ', low_bid
-#     )
-
-
-# def logger(pair, ask, bid, spread):
-#     print_time = datetime.datetime.now().strftime('%H:%M:%S %Y-%m-%d')
-#     print(
-#         '\n',pair,' @ ',print_time , 
-#         '\n--> Selling Price: ', ask, 
-#         '\n--> Buying Price: ', bid, 
-#         '\n--> Spread: ', spread
-#     )
-
-
-# def main():
-#    
-     
-#     while exit == False:
-#         try:            
-#             ticker = connection.get_ticker(pair=currency_pair)
-#             ask_price = round(float(ticker['ask']))
-#             bid_price = round(float(ticker['bid']))
-#             gap = ask_price - bid_price
-
-#             asks.append(ask_price)
-#             bids.append(bid_price)
-
-
-#             #   ****************** Print Out The Arrays for bids and asks ****************
-#             # print('Asks: ',asks,'\nBids: ',bids)
-
-
-            
-#             # Starts after data initialized
-#             if len(asks) > data_flow:
-#                 if initilized == False:
-#                     print('Data Reached Cap | Now Initialized \n --> Bot Now Active')
-#                     logger(currency_pair, ask_price, bid_price, gap)
-#                     calculations(asks, bids)
-#                     initilized = True
-                
-#                 asks.pop(0)
-#                 bids.pop(0)
-                
-#                 if asks[-2] != ask_price or bids[-2] != bid_price:
-#                     logger(currency_pair, ask_price, bid_price, gap)
-#                     calculations(asks, bids)
-                    
-#             else:                
-#                 print('Initializing... ', data_flow + 1 - len(asks))
-            
-
-            
-
-#         except Exception as e:
-#             print(e)   
-#         time.sleep(period)
-    
-     
-
-
-# if __name__ == '__main__':
-#     main()
 
